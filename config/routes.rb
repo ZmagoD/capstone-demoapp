e151bbda9c754afd89f0d7558fe5141c9d28201a
ZmagoD <zmago_devetak@yahoo.com>
2017-01-22 19:17:37 +0100

    added comming soon page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+	get '/ui' => 'ui#index'
+	get '/ui#' => 'ui#index'
+	root 'ui#index'
+
 	scope :api, defaults: { format: :json } do
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
