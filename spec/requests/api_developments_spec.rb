201faedaa6a6bf3c75390049384a24f90a0a83e8
ZmagoD <zmago_devetak@yahoo.com>
2017-01-22 18:54:02 +0100

    final commit

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -38,6 +38,8 @@ RSpec.describe "ApiDevelopments", type: :request do
 		  get (bar_path(obj.id))
 		  expect(response).to have_http_status(:ok)
 		  expect(parsed_body['name']).to eq('Test')
+		  expect(parsed_body).to include('created_at')
+		  expect(parsed_body).to include('id' => obj.id.to_s)
 	  end
   end
 end
